901ff2c44963d7468e04749b85d39bc2d74d6cbd
Franklin Garcia <franklin.g.mendoza@gmail.com>
2023-07-27 09:38:22 -0400

    changing list to string

--- main.tf
+++ main.tf
@@ -176,7 +176,7 @@ resource "aws_security_group_rule" "worker_sg" {
   from_port                = 0 - 65536
   to_port                  = 0 - 65536
   protocol                 = "all"
-  source_security_group_id = [aws_security_group.eks_sg.id]
+  source_security_group_id = aws_security_group.eks_sg.id
   security_group_id        = module.eks.cluster_primary_security_group_id
 }
 
